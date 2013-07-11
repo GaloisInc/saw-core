c47953327fa3f6707c603d781fffa281c0508825
Aaron Tomb <atomb@galois.com>
2013-07-11 10:27:09 -0700

    Expand saw-export API slightly.
    Mostly add comments about what we should add.

--- c-export/src/Verifier/SAW/ForeignExport.hs
+++ c-export/src/Verifier/SAW/ForeignExport.hs
@@ -1,31 +1,79 @@
-module Verifier.SAW.ForeignExport where
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+-- Allow type signatures to be left out, because they must be
+-- duplicated in the foreign export lines.
 
-import Control.Monad
-import Foreign.C.String
+module Verifier.SAW.ForeignExport() where
+
+import Foreign.C
 import Foreign.StablePtr
 
 import Verifier.SAW.SharedTerm
 import Verifier.SAW.TypedAST
 
 expModule :: Module
-expModule = emptyModule (mkModuleName ["SAW", "Export"])
+expModule = emptyModule (mkModuleName ["Verifier", "SAW", "ForeignExport"])
 
-saw_new_context :: IO (StablePtr (SharedContext s))
 saw_new_context = mkSharedContext expModule >>= newStablePtr
 
-saw_free :: StablePtr (SharedContext s) -> IO ()
 saw_free = freeStablePtr
 
-saw_bool_type :: StablePtr (SharedContext s) -> IO (StablePtr (SharedTerm s))
-saw_bool_type = deRefStablePtr >=> scBoolType >=> newStablePtr
+with_context     f p = deRefStablePtr p >>= f >>= newStablePtr
+--with_term        f p = deRefStablePtr p >>= f >>= newStablePtr
+with_term_string f p = deRefStablePtr p >>= f >>= newCString
+
+{-
+bv_binop f scp ap bp = do
+  sc <- derefStablePtr scp
+  a <- derefStablePtr ap
+  ty <- scTypeOf sc a
+  b <- derefStablePtr bp
+  newStablePtr =<< scApplyAll sc f [w, a, b]
+
+bv_unop f scp ap = do
+  sc <- derefStablePtr scp
+  a <- derefStablePtr ap
+  ty <- scTypeOf sc a
+  newStablePtr =<< scApplyAll sc f [w, a]
+-}
+
+saw_bool_type   = with_context scBoolType
+saw_nat_type    = with_context scNatType
+saw_bv_type p w = with_context (\sc -> scBitvector sc (fromIntegral w)) p
+
+saw_nat_const  p n = with_context (\sc -> scNat sc (fromIntegral n)) p
+saw_bool_const p b = with_context (\sc -> scBool sc (b /= 0)) p
 
 saw_pretty_print :: StablePtr (SharedTerm s) -> IO CString
-saw_pretty_print = deRefStablePtr >=> (return . show) >=> newCString
+saw_pretty_print = with_term_string (return . show)
+
+-- To add:
+--  * bitvector: +, -, *, /, >>, <<, fromNat, and, or, xor, negate, eq,
+--               append, <, >
+--  * boolean: and, or, xor, not
+--  * nat: +, *, eq, <, >
+--  * AIG: read_aig, write_aig
+--  * SAWCore: read_sawcore_shared, write_sawcore_shared
+--  * Proof: prove_abc
 
 foreign export ccall saw_new_context :: IO (StablePtr (SharedContext s))
 foreign export ccall saw_free :: StablePtr (SharedContext s) -> IO ()
 
 foreign export ccall saw_bool_type :: StablePtr (SharedContext s)
+                                   -> IO (StablePtr (SharedTerm s))
+
+foreign export ccall saw_nat_type :: StablePtr (SharedContext s)
+                                  -> IO (StablePtr (SharedTerm s))
+
+foreign export ccall saw_bv_type :: StablePtr (SharedContext s)
+                                 -> CInt
+                                 -> IO (StablePtr (SharedTerm s))
+
+foreign export ccall saw_bool_const :: StablePtr (SharedContext s)
+                                    -> CInt
+                                    -> IO (StablePtr (SharedTerm s))
+
+foreign export ccall saw_nat_const :: StablePtr (SharedContext s)
+                                   -> CInt
                                    -> IO (StablePtr (SharedTerm s))
 
 foreign export ccall saw_pretty_print :: StablePtr (SharedTerm s)
--- c-export/test/test_export.c
+++ c-export/test/test_export.c
@@ -7,10 +7,13 @@ int main(int argc, char **argv) {
     hs_init(&argc, &argv);
     HsStablePtr sc = saw_new_context();
     HsStablePtr bt = saw_bool_type(sc);
+    HsStablePtr nc = saw_nat_const(sc, 12839);
     printf("Shared context: %p\n", sc);
     printf("Bool type: %p\n", bt);
     printf("Bool type pretty-printed: %s\n", (char *)saw_pretty_print(bt));
+    printf("Nat const pretty-printed: %s\n", (char *)saw_pretty_print(nc));
     saw_free(bt);
+    saw_free(nc);
     saw_free(sc);
     hs_exit();
 }
