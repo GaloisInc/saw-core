45fedbfa8b37a73f517fb105cb3bc3fa640fd2db
Aaron Tomb <atomb@galois.com>
2013-07-11 09:12:42 -0700

    Add Makefile and fix build for saw-export test.

--- c-export/test/test_export.c
+++ c-export/test/test_export.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include "Export_stub.h"
+#include "Verifier/SAW/ForeignExport_stub.h"
 #include "saw.h"
 
 int main(int argc, char **argv) {
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
     HsStablePtr bt = saw_bool_type(sc);
     printf("Shared context: %p\n", sc);
     printf("Bool type: %p\n", bt);
-    printf("Bool type pretty-printed: %s\n", saw_pretty_print(bt));
+    printf("Bool type pretty-printed: %s\n", (char *)saw_pretty_print(bt));
     saw_free(bt);
     saw_free(sc);
     hs_exit();
