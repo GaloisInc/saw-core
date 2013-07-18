ea197d8c60ba1f56b116f3f36765a9c639dad21b
Joe Hendrix <jhendrix@galois.com>
2013-07-18 09:52:44 -0700

    Extend range of types that can be bitblasted by SAWCore.

--- src/Verifier/SAW/BitBlast.hs
+++ src/Verifier/SAW/BitBlast.hs
@@ -19,13 +19,13 @@ module Verifier.SAW.BitBlast
   ) where
 
 import Control.Applicative
+import Control.Lens
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Error
 import Data.IORef
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Traversable (traverse)
 import qualified Data.Vector as V
 import qualified Data.Vector.Storable as LV
 
@@ -35,10 +35,15 @@ import Verifier.SAW.TypedAST
 import Verinf.Symbolic.Lit
 
 data BValue l
-    = BBool l
-    | BVector (LitVector l)
+    = BBool !l
+    | BVector (V.Vector (BValue l))
     | BTuple [BValue l]
     | BRecord (Map FieldName (BValue l))
+
+
+lvVector :: LV.Storable l => LitVector l -> BValue l
+lvVector lv = BVector v 
+  where v = V.generate (LV.length lv) (\i -> BBool (lv LV.! i)) 
 
 bTupleSelect :: Int -> BValue l -> BValue l
 bTupleSelect n (BTuple vs) = vs !! (n - 1)
@@ -48,11 +53,15 @@ bRecordSelect :: FieldName -> BValue l -> BValue l
 bRecordSelect n (BRecord (Map.lookup n -> Just v)) = v
 bRecordSelect _ _ = error "expected Record"
 
+
+lvFromV :: LV.Storable l => V.Vector l -> LV.Vector l
+lvFromV v = LV.generate (V.length v) (v V.!)
+  
 flattenBValue :: LV.Storable l => BValue l -> LitVector l
 flattenBValue (BBool l) = LV.singleton l
-flattenBValue (BVector v) = v
-flattenBValue (BTuple vs) = LV.concat (map flattenBValue vs)
-flattenBValue (BRecord vm) = LV.concat (map flattenBValue (Map.elems vm))
+flattenBValue (BVector v) = LV.concat (flattenBValue <$> V.toList v)
+flattenBValue (BTuple vs) = LV.concat (flattenBValue <$> vs)
+flattenBValue (BRecord vm) = LV.concat (flattenBValue <$> Map.elems vm)
 
 type BBErr = String
 type BBMonad = ErrorT BBErr IO
@@ -93,7 +102,7 @@ bitBlastWith bc t0 = runErrorT (go [] t0)
         newVars :: SharedTerm s -> BBMonad (BValue l)
         newVars (asBoolType -> Just ()) = liftIO $ BBool <$> beMakeInputLit be
         newVars (asBitvectorType -> Just n) = liftIO $
-          BVector <$> LV.replicateM (fromInteger n) (beMakeInputLit be)
+          lvVector <$> LV.replicateM (fromInteger n) (beMakeInputLit be)
         newVars (asTupleType -> Just ts) = BTuple <$> traverse newVars ts
         newVars (asRecordType -> Just tm) = BRecord <$> traverse newVars tm
         newVars t = fail $ "bitBlast: unsupported argument type: " ++ show t
@@ -103,6 +112,7 @@ bitBlastWith bc t0 = runErrorT (go [] t0)
           | ident == "Prelude.False" = return (BBool (beFalse be))
           | ident == "Prelude.True"  = return (BBool (beTrue be))
         go ls (asLambda -> Just (_, ty, body)) = do
+          liftIO $ putStrLn "New Lambda var" 
           v <- newVars ty
           go (v : ls) body
         go ls t@(STApp termidx tf) = do
@@ -112,12 +122,12 @@ bitBlastWith bc t0 = runErrorT (go [] t0)
           case Map.lookup termidx m of
             Just r -> return r
             Nothing
-              | FTermF (ExtCns ec) <- tf ->
+              | FTermF (ExtCns ec) <- tf -> do
                   pushNew =<< newVars (ecType ec)
               | LocalVar i _ty <- tf -> return (ls !! i)
               | (STApp _ (FTermF (GlobalDef ident)), xs) <- asApplyAll t ->
                  case Map.lookup ident opTable of
-                   Just op -> pushNew =<< op be go' xs
+                   Just f -> pushNew =<< f be go' xs
                    Nothing -> fail $ "unsupported operator " ++ show ident
               | FTermF (TupleValue ts) <- tf ->
                   pushNew =<< (BTuple <$> traverse go' ts)
@@ -127,10 +137,9 @@ bitBlastWith bc t0 = runErrorT (go [] t0)
                   pushNew =<< (bTupleSelect n <$> go' t')
               | FTermF (RecordSelector t' n) <- tf ->
                   pushNew =<< (bRecordSelect n <$> go' t')
-              | FTermF (ArrayValue ty es) <- tf
-              , Just () <- asBoolType ty -> do
-                  xs <- V.mapM (go' >=> asBBool) es
-                  pushNew (BVector (LV.fromList (V.toList xs)))
+              | FTermF (ArrayValue _ty es) <- tf -> do
+                  xs <- V.mapM go' es
+                  pushNew $ BVector xs
               | otherwise ->
                   fail $ "unsupported expression: " ++ take 20 (show t)
 
@@ -179,14 +188,18 @@ opTable =
     , ("slice", bvSliceOp)
     ]
 
-bvBinOp :: (BitEngine l -> LitVector l -> LitVector l -> IO (LitVector l)) -> BValueOp s l
+bvBinOp :: LV.Storable l
+        => (BitEngine l -> LitVector l -> LitVector l -> IO (LitVector l))
+        -> BValueOp s l
 bvBinOp f be eval [_, mx, my] =
     do x <- asBVector =<< eval mx
        y <- asBVector =<< eval my
-       liftIO $ BVector <$> f be x y
+       liftIO $ lvVector <$> f be x y
 bvBinOp _ _ _ _ = wrongArity "binary op"
 
-bvRelOp :: (BitEngine l -> LitVector l -> LitVector l -> IO l) -> BValueOp s l
+bvRelOp :: (LV.Storable l)
+        => (BitEngine l -> LitVector l -> LitVector l -> IO l)
+        -> BValueOp s l
 bvRelOp f be eval [_, mx, my] =
     do x <- asBVector =<< eval mx
        y <- asBVector =<< eval my
@@ -210,7 +223,7 @@ bvNatOp :: LV.Storable l => BValueOp s l
 bvNatOp be _ [mw, mx] = do
   w <- asBNat mw
   x <- asBNat mx
-  return (BVector (beVectorFromInt be (fromIntegral w) x))
+  return (lvVector (beVectorFromInt be (fromIntegral w) x))
 bvNatOp _ _ _ = wrongArity "bvNat op"
 
 notOp :: BValueOp s l
@@ -223,36 +236,44 @@ appendOp :: LV.Storable l => BValueOp s l
 appendOp _ eval [_, _, _, mx, my] =
     do x <- asBVector =<< eval mx
        y <- asBVector =<< eval my
-       return (BVector ((LV.++) x y))
+       return (lvVector ((LV.++) x y))
 appendOp _ _ _ = wrongArity "append op"
 
 singleOp :: LV.Storable l => BValueOp s l
 singleOp _ eval [_, mx] =
     do x <- asBBool =<< eval mx
-       return (BVector (LV.singleton x))
+       return (lvVector (LV.singleton x))
 singleOp _ _ _ = wrongArity "single op"
 
 iteOp :: (Eq l, LV.Storable l) => BValueOp s l
-iteOp be eval [_, mb, mx, my] =
-    do b <- asBBool =<< eval mb
-       case () of
-         _ | b == beTrue be -> eval mx
-           | b == beFalse be -> eval my
-           | otherwise ->
-               do bx <- eval mx
-                  by <- eval my
-                  case (bx, by) of
-                    (BBool x, BBool y) -> liftIO (fmap BBool (beMux be b x y))
-                    (BVector x, BVector y) ->
-                        liftIO (fmap BVector (beIteVector be b (return x) (return y)))
-                    _ -> fail "malformed arguments"
+iteOp be eval [_, mb, mx, my] = do
+  b <- asBBool =<< eval mb
+  case () of
+    _ | b == beTrue be -> eval mx
+      | b == beFalse be -> eval my
+      | otherwise -> do
+            x <- eval mx
+            y <- eval my
+            liftIO $ iteFn x y
+          where iteFn (BBool x) (BBool y) = BBool <$> beMux be b x y
+                iteFn (BVector x) (BVector y)
+                  | V.length x == V.length y
+                  = BVector <$> V.zipWithM iteFn x y
+                iteFn (BTuple x) (BTuple y)
+                  | length x == length y
+                  = BTuple <$> zipWithM iteFn x y
+                iteFn (BRecord x) (BRecord y)
+                  | Map.keys x == Map.keys y
+                  = fmap BRecord $ sequenceOf traverse 
+                                 $ Map.intersectionWith iteFn x y
+                iteFn _ _ = fail "malformed arguments."
 iteOp _ _ _ = wrongArity "ite op"
 
 bvTruncOp :: (Eq l, LV.Storable l) => BValueOp s l
 bvTruncOp be eval [_, mj, mx] =
     do j <- asBNat mj
        x <- asBVector =<< eval mx
-       return (BVector (beTrunc be (fromIntegral j) x))
+       return (lvVector (beTrunc be (fromIntegral j) x))
 bvTruncOp _ _ _ = wrongArity "trunc op"
 
 bvUExtOp :: (Eq l, LV.Storable l) => BValueOp s l
@@ -260,7 +281,7 @@ bvUExtOp be eval [mi, mj, mx] =
     do i <- asBNat mi
        j <- asBNat mj
        x <- asBVector =<< eval mx
-       return (BVector (beZext be (fromIntegral (i + j)) x))
+       return (lvVector (beZext be (fromIntegral (i + j)) x))
 bvUExtOp _ _ _ = wrongArity "UExt op"
 
 bvSExtOp :: (Eq l, LV.Storable l) => BValueOp s l
@@ -268,7 +289,7 @@ bvSExtOp be eval [mi, mj, mx] =
     do i <- asBNat mi
        j <- asBNat mj
        x <- asBVector =<< eval mx
-       return (BVector (beSext be (fromIntegral (i + j + 1)) x))
+       return (lvVector (beSext be (fromIntegral (i + j + 1)) x))
 bvSExtOp _ _ _ = wrongArity "SExt op"
 
 bvGetBitOp :: (Eq l, LV.Storable l) => BValueOp s l
@@ -288,7 +309,7 @@ bvSliceOp _ eval [_, mi, mn, _, mx] =
     do i <- fromInteger <$> asBNat mi
        n <- fromInteger <$> asBNat mn
        x <- asBVector =<< eval mx
-       return (BVector (LV.take n (LV.drop i x)))
+       return (lvVector (LV.take n (LV.drop i x)))
 bvSliceOp _ _ _ = wrongArity "slice op"
 
 bvSignedShrOp :: (Eq l, LV.Storable l) => BValueOp s l
@@ -297,7 +318,7 @@ bvSignedShrOp be eval [_, xt, nt] =
        n <- asBNat nt
        let w = LV.length x
            nv = beVectorFromInt be w n
-       liftIO (fmap BVector (beSignedShr be x nv))
+       liftIO (fmap lvVector (beSignedShr be x nv))
 bvSignedShrOp _ _ _ = wrongArity "SShr op"
 
 bvUnsignedShrOp :: (Eq l, LV.Storable l) => BValueOp s l
@@ -306,7 +327,7 @@ bvUnsignedShrOp be eval [_, xt, nt] =
        n <- asBNat nt
        let w = LV.length x
            nv = beVectorFromInt be w n
-       liftIO (fmap BVector (beUnsignedShr be x nv))
+       liftIO (fmap lvVector (beUnsignedShr be x nv))
 bvUnsignedShrOp _ _ _ = wrongArity "SShr op"
 
 bvShlOp :: (Eq l, LV.Storable l) => BValueOp s l
@@ -315,14 +336,14 @@ bvShlOp be eval [_, xt, nt] =
        n <- asBNat nt
        let w = LV.length x
            nv = beVectorFromInt be w n
-       liftIO (fmap BVector (beShl be x nv))
+       liftIO (fmap lvVector (beShl be x nv))
 bvShlOp _ _ _ = wrongArity "Shl op"
 
 ----------------------------------------------------------------------
 -- Destructors for BValues
 
-asBVector :: BValue l -> BBMonad (LitVector l)
-asBVector (BVector lv) = return lv
+asBVector :: LV.Storable l => BValue l -> BBMonad (LitVector l)
+asBVector (BVector v) = lvFromV <$> traverse asBBool v
 asBVector _ = fail "expected Vector"
 
 asBBool :: BValue l -> BBMonad l
