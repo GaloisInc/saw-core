f8cbef9cb019a87ff2488e4c23d08fca3a6abaf1
Aaron Tomb <atomb@galois.com>
2013-08-02 10:13:23 -0700

    Update build scripts for saw-export.
    Can now successfully build test_export.c without using GHC.

---
+++ c-export/build-tools/build-test.sh
@@ -0,0 +1,1 @@
+gcc -I dist/build -I cbits -L . -o sctest test/test_export.c -lsawcore -liconv
--- c-export/build-tools/make-lib.sh
+++ c-export/build-tools/make-lib.sh
@@ -1,11 +1,13 @@
 F=__liblist
 SCT=libsawcoretemp.a
+RTS=`ghc --print-libdir`/libHSrts.a
 rm -f $F
 for l in `cat build-tools/base-deps` ; do
   ls `ghc --print-libdir`/$l/HS*.o >> $F
 done
 ls cabal-dev/lib/*/ghc-7.6.3/HS*.o >> $F
 ar -r ${SCT} `cat $F`
-libtool -static -o libsawcore.a cabal-dev/lib/abcBridge-*/ghc-*/libabc.a ${SCT}
+ABC=`ls cabal-dev/lib/abcBridge-*/ghc-*/libabc.a`
+libtool -static -o libsawcore.a ${RTS} ${ABC} ${SCT}
 rm -f ${SCT}
 rm -f $F
