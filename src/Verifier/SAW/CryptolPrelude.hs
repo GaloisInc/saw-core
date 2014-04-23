1cd3897fe4862fe7eedf874c185e0ba15302ad19
Brian Huffman <huffman@galois.com>
2014-04-23 13:46:21 -0700

    Add separate Cryptol.sawcore prelude for cryptol primitives

---
+++ src/Verifier/SAW/CryptolPrelude.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Verifier.SAW.CryptolPrelude
+  ( Module
+  , module Verifier.SAW.CryptolPrelude
+  ) where
+
+import Verifier.SAW.Prelude
+import Verifier.SAW.ParserUtils
+import Verifier.SAW.SharedTerm
+
+$(runDecWriter $ do
+    prelude <- defineImport [|preludeModule|] preludeModule
+    cryptol <- defineModuleFromFile [prelude] "cryptolModule" "prelude/Cryptol.sawcore"
+    declareSharedModuleFns "Cryptol" (decVal cryptol)
+ )
